78da32fa7cdf0e60e212799c3c0807cfa79a8cb8
Leolai80047 <leolai80047@gmail.com>
2024-08-06 21:31:11 +0800

    [Add] apply edge to edge

--- app/src/main/java/com/leodemo/chat_room/MainActivity.kt
+++ app/src/main/java/com/leodemo/chat_room/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -109,7 +110,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         speechRecognizerManager = SpeechRecognizerManager(this, speechListener)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             ChatRoomTheme {
                 ChatRoom(
@@ -151,7 +152,6 @@ private fun ChatRoom(
         content = { paddingValues ->
             Column(
                 Modifier
-                    .navigationBarsPadding()
                     .background(MaterialTheme.colorScheme.background)
                     .padding(paddingValues)
             ) {
